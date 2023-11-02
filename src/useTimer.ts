b137edfcdfe377dd155c357a603c69619336b0ff
Gustavo Barbosa <gusbdev@gmail.com>
2023-11-02 03:28:00 -0300

    feat: initial hook functions

---
+++ src/useTimer.ts
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Timer from "./index";
+
+const useTimer = () => {
+  const [timer] = useState(new Timer());
+
+  const measureAsync = async (fn: () => Promise<any>) => {
+    timer.start();
+    try {
+      await fn();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      timer.stop();
+      return timer.getElapsedTime();
+    }
+  };
+
+  const measureSync = (fn: () => any) => {
+    timer.start();
+    try {
+      fn();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      timer.stop();
+      return timer.getElapsedTime();
+    }
+  };
+
+  return { measureAsync, measureSync };
+};
+
+export default useTimer;
